40e055dc9cf4dffd79ff90bd09aa569418d87214
fuadhasanemon <fuadhasanemon8@gmail.com>
2022-05-31 04:26:31 +0600

    some modifications

--- taskTwo/assets/js/main.js
+++ taskTwo/assets/js/main.js
@@ -56,7 +56,8 @@ Author: GrayGrids
 $('.responsive').slick({
     dots: true,
     infinite: true,
-    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
     slidesToShow: 1,
     slidesToScroll: 1,
     responsive: [
